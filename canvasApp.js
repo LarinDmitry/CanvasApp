4c875b9ab6910b726bfa0285e2d49bdf9b66cfd2
LarinDmitry <svk6c6zk1@gmail.com>
2018-04-17 00:13:20 +0300

    paint is ready for use

--- canvasApp.js
+++ canvasApp.js
@@ -1,7 +1,75 @@
 var canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d'),
+    isMouseDown = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+/*
 //Code
+//клик=круг
+////////////////////////////////////////////////////////
+canvas.addEventListener('mousedown', function (e) {
+   ctx.beginPath();
+   ctx.arc(e.clientX, e.clientY, 30, 0 , Math.PI*2);
+   ctx.fill();
+});
+////////////////////////////////////////////////////////
+
+
+//рисовалка без клика
+////////////////////////////////////////////////////////
+canvas.addEventListener('mousemove', function (e) {
+   ctx.beginPath();
+   ctx.arc(e.clientX, e.clientY, 30, 0 , Math.PI*2);
+   ctx.fill();
+});
+////////////////////////////////////////////////////////
+
+//рисовалка по клику
+////////////////////////////////////////////////////////
+
+canvas.addEventListener('mousedown', function (e) {
+    isMouseDown=true;
+});
+
+canvas.addEventListener('mouseup', function (e) {
+    isMouseDown=false;
+});
+
+canvas.addEventListener('mousemove', function (e) {
+    if (isMouseDown) {
+        ctx.beginPath();
+        ctx.arc(e.clientX, e.clientY, 10, 0 , Math.PI*2);
+        ctx.fill();
+    }
+});
+
+////////////////////////////////////////////////////////
+
+//рисовалка по клику без гепа
+////////////////////////////////////////////////////////
+ctx.lineWidth=10*2;
+canvas.addEventListener('mousedown', function (e) {
+    isMouseDown=true;
+});
+
+canvas.addEventListener('mouseup', function (e) {
+    isMouseDown=false;
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', function (e) {
+    if (isMouseDown) {
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(e.clientX, e.clientY, 10, 0 , Math.PI*2);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+});
+////////////////////////////////////////////////////////*/
+
